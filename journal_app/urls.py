f52f545f4f3e93c43b3115cff9988b1685bdcc8a
Yaroslav Plotnickov <plotnickov.jaroslav@yandex.ru>
2019-12-28 17:59:50 +0300

    Created forms

--- journal_app/urls.py
+++ journal_app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'), #Main page
     url(r'^topics/$', views.topics, name='topics'), #Topics page
     url(r'^topics/(?P<topic_id>\d+)$', views.topic, name='topic'), #Details topic page
-    url(r'^new_topic/$', views.new_topic, name='new_topic'), #Topics page
+    url(r'^create_topic/$', views.create_topic, name='create_topic'), #Create topic page
+    url(r'^create_entry/(?P<topic_id>\d+)/$', views.create_entry, name='create_entry'), #Create entry page
+    url(r'^edit_entry/(?P<topic_id>\d+)/$', views.edit_entry, name='edit_entry'), #Create topic page
 ]
 
--- journal_app/views.py
+++ journal_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import forms
+from .forms import TopicForm, EntryForm
 from .models import Topic
 
 # Create your views here.
@@ -22,6 +22,44 @@ def topic(request, topic_id):
     ordered_entries = current_topic.entry_set.order_by('-date')
     context = {'topic': current_topic, 'entries': ordered_entries}
     return render(request, 'journal_app/topic.html', context)
+
+def create_topic(request):
+    """Define new topic"""
+    if request.method != 'POST':
+        """Empty form"""
+        form = TopicForm()
+    else:
+        """Send data for new topic"""
+        form = TopicForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+
+    context = {'form': form}
+    return render(request, 'journal_app/create_topic.html', context)
+
+def create_entry(request, topic_id):
+    """Add new entry to particular topic"""
+    choosen_topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        """Empty form"""
+        form = EntryForm()
+    else:
+        """Send data for new topic"""
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = choosen_topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+
+    context = {'topic': choosen_topic, 'form': form}
+    return render(request, 'journal_app/create_entry.html', context)
+
+
+    
+
 
 
 
